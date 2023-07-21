8e0ebcde9bea6dfdbe406969af05fc000be81160
Lakshmi28Manasa <lakshmimanasa002@gmail.com>
2023-07-21 07:07:03 +0530

    Section 2 : Register/Configure classes into Program class. IMP

---
+++ Mango.Web/Service/CouponService.cs
@@ -0,0 +1,43 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+
+namespace Mango.Web.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly ICouponService _couponService;
+        public CouponService(ICouponService couponService)
+        {
+            _couponService=couponService;
+        }
+        public Task<ResponseDto?> CreateCouponByIdAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> DeleteCouponByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetAllCouponAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetCouponAsync(string couponId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetCouponByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> UpdateCouponByIdAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
